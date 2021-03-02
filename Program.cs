32eff5774658927ff26a567d84fde3a98afe1abf
Haor Shiu <haor.shiu@jipo.com.tw>
2021-03-02 16:21:49 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 5, 8, 13 };
+            var max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max) max = nums[i];
+            }
+            Console.WriteLine("Maximum number is " + max.ToString());
+            Console.ReadLine();
+        }
+    }
+}
