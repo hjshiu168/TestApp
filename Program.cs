0018a611ed8ef9ce4e2ffa948f4cd168a7b6b6ae
Haor Shiu <haor.shiu@jipo.com.tw>
2021-03-02 16:46:40 +0800

    update max number

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[] { 1, 3, 5, 8, 13 };
+            var max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max) max = nums[i];
+            }
+            Console.WriteLine("Maximum number is " + max.ToString());
         }
     }
 }
