0abb780f18179d68c942d3bc4b002309858d89a2
Haor Shiu <haor.shiu@jipo.com.tw>
2021-03-02 16:46:02 +0800

    init added

--- Program.cs
+++ Program.cs
@@ -6,14 +6,6 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 1, 3, 5, 8, 13 };
-            var max = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (nums[i] > max) max = nums[i];
-            }
-            Console.WriteLine("Maximum number is " + max.ToString());
-            Console.ReadLine();
         }
     }
 }
